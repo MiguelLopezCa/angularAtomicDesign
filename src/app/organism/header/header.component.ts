bcf4338e1b330c20f5531be90d42e33395765440
MiguelLopezCa <migantlopez@uniboyaca.edu.co>
2023-11-13 23:00:23 -0500

    css page 1

--- src/app/organism/header/header.component.ts
+++ src/app/organism/header/header.component.ts
@@ -1,6 +1,5 @@
   import { Component, EventEmitter, Input, Output } from '@angular/core';
   import { Router } from '@angular/router';
-
   @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
